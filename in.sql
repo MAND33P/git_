91ad1892465ec3d9e7bd0756778496986dc24193
mandeep <13114425man@gmail.com>
2021-08-10 23:50:48 +0530

    h

---
+++ in.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Employee  
+(  
+EmployeeID number(10),  
+FirstName varchar2(255),  
+LastName varchar2(255),  
+Email varchar2(255),  
+AddressLine varchar2(255),  
+City varchar2(255)  
+);  
